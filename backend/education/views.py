c58ec71f47c59235de389e027e8840c5e2a6d23a
Roberts Ivanovs <roberts.ivanovs1999@gmail.com>
2020-08-21 23:56:12 +0300

    Backend: Views: Added Programme and University CRUD views

--- backend/education/views.py
+++ backend/education/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from education.serializers import ProgrammeSerializer, UniversitySerializer
+from education.models import Programme, University
+
+
+class ProgrammeViewset(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing Programme instances.
+    """
+
+    serializer_class = ProgrammeSerializer
+    queryset = Programme.objects.all()
+
+
+class UniversityViewset(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing University instances.
+    """
+
+    serializer_class = UniversitySerializer
+    queryset = University.objects.all()
