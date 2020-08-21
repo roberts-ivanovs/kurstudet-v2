511b0aa4cc25769da170dffb47d5021ff7689912
Roberts Ivanovs <roberts.ivanovs1999@gmail.com>
2020-08-21 21:14:34 +0300

    Backend: DB: Updated choice fields for Programme

--- backend/education/models.py
+++ backend/education/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class University(models.Model):
@@ -19,31 +20,28 @@ class Programme(models.Model):
     The accumulation of all data that's associated for a given study programme
     """
 
-    DEGREES = [
-        ("CL", "College"),
-        ("BA", "Bachelors"),
-        ("MA", "Masters"),
-        ("PHD", "Doctors"),
-    ]
+    class Degrees(models.TextChoices):
+        COLLEGE = "CL", _("College")
+        BACHELOR = "BA", _("Bachelors")
+        MASTER = "MA", _("Masters")
+        DOCTOR = "PHD", _("Doctors")
 
-    LEARNING_TYPES = [
-        ("IRL", "Real life: Day, Night"),
-        ("SRL", "Semi-real life: Weekends, Sessions, E-studies"),
-        ("EKS", "Self taught: Extramural."),
-    ]
+    class LearningTypes(models.TextChoices):
+        REAL_LIFE = "IRL", _("Real life: Day, Night")
+        DISTANT = "DIS", _("Semi-real life: Weekends, Sessions, E-studies")
+        EXTRAMURAL = "EKS", _("Self taught")
 
-    # Core information
     name = models.CharField(max_length=100, null=True, blank=True)
     university = models.ForeignKey(University, on_delete=models.PROTECT)
-    degree = models.CharField(max_length=100, choices=DEGREES)
+    degree = models.CharField(max_length=100, choices=Degrees.choices)
     duration_years = models.DecimalField(max_digits=4, decimal_places=2)
     faculty = models.CharField(max_length=100, null=True, blank=True)
-
-    website = models.URLField(max_length=300, null=True, blank=True)
-
-    description = models.TextField(null=True, blank=True)
-    learning_type = models.CharField(max_length=100, choices=LEARNING_TYPES)
+    learning_type = models.CharField(max_length=100, choices=LearningTypes.choices)
     full_time = models.BooleanField()
+    budget_places = models.IntegerField()
+    total_places = models.IntegerField()
+    website = models.URLField(max_length=300, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
