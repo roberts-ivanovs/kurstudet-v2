cc0b4d164008a6fa9176b739ba9d9eab909e1e46
Roberts Ivanovs <roberts.ivanovs1999@gmail.com>
2020-08-22 19:17:28 +0300

    Backend: URL: `core` app urls added to global urlconfig

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/education/', include('education.urls'))
+    path('api/education/', include('education.urls')),
+    path('api/core/', include('core.urls'))
 ]
