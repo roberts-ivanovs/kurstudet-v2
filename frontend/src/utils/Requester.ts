71a9a86220215f5940777aabe6958add5a17b899
Roberts Ivanovs <roberts.ivanovs1999@gmail.com>
2020-08-23 19:03:24 +0300

    Frontend: Utils: Created basic Requester impl

---
+++ frontend/src/utils/Requester.ts
@@ -0,0 +1,33 @@
+
+import Axios, { AxiosStatic } from 'axios';
+
+class Requester {
+    axios: AxiosStatic;
+    authHeader: { Authorization: string };
+
+    constructor() {
+        this.axios = Axios;
+        this.authHeader = { Authorization: "" };
+    }
+
+    post = async (url: string, data = {}): Promise<any> => {
+        const response = await this.axios
+            .post(url, data, { headers: this.authHeader })
+            .catch((error) => error.response);
+        return response.data;
+    };
+
+    get = async (url: string, params = ''): Promise<any> => {
+        const response = await this.axios.get(url, {
+            params,
+            headers: this.authHeader
+        });
+        return response.data;
+    };
+
+    setAuthHeader = (token: string) => {
+        this.authHeader = { 'Authorization': `Token ${token}` };
+    }
+}
+const RequesterInstance = new Requester();
+export default RequesterInstance;
