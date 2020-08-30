5b9dcc12f6f1ceb6ffa482208c0c5bb867740355
Roberts Ivanovs <roberts.ivanovs1999@gmail.com>
2020-08-30 13:01:58 +0300

    Backend: Registered forum backend models to views

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 
-# from knox import views as knox_views
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
--- backend/forum/views.py
+++ backend/forum/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from forum.serializers import AnswerSerializer, TagSerializer, QuestionSerializer
+from forum.models import Answer, Question, Tag
 
-# Create your views here.
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
