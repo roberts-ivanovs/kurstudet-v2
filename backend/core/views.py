b97304c4c95176269a9118f2b76af3c5514bd228
Roberts Ivanovs <roberts.ivanovs1999@gmail.com>
2020-08-30 18:38:57 +0300

    Misc: Added docstrings to `core` app viewsets
    (cherry picked from commit fa1c809fbf7115be8b46d90d9462b209aa1f0005)

--- backend/core/views.py
+++ backend/core/views.py
@@ -14,7 +14,42 @@ from core.permissions import CanOnlyViewItself
 
 class UserViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    CRUD `core.models.User` items
+
+    Permissions
+    ----------
+    - Anyone who has the model permissions via Djangos contenttype app
+    ----------
+
+    Response item
+    ----------
+    {
+        "id": int,
+        "last_login": str,
+        "is_superuser": bool,
+        "username": str,
+        "first_name": str,
+        "last_name": str,
+        "email": str,
+        "is_staff": bool,
+        "is_active": bool,
+        "date_joined": str,
+        "groups": [int, ..],
+        "user_permissions": [int, ..],
+    }
+
+    Method calls
+    ----------
+    :url - base URL
+    :id - primary key for the model
+    :obj - a single response item
+
+    GET     ":url"      -> [:obj, :obj, ... ]                   | status 200
+    GET     ":url/:id"  -> :obj                                 | status 200
+    POST    ":url"      -> :obj (the newly created object)      | status 201
+    PUT     ":url/:id"  -> :obj (the newly updated object)      | status 200
+    DELETE  ":url"      -> { "message": str }                   | status 204
+    DELETE  ":url/:id"  -> { "message": str }                   | status 204
     """
 
     queryset = User.objects.all().order_by("-date_joined")
@@ -24,7 +59,33 @@ class UserViewSet(viewsets.ModelViewSet):
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows groups to be viewed or edited.
+    CRUD `django.contrib.auth.models.Group` items
+
+    Permissions
+    ----------
+    - Anyone who has the model permissions via Djangos contenttype app
+    ----------
+
+    Response item
+    ----------
+    {
+        "id": int,
+        "name": str,
+        "permissions": [int, ..],
+    }
+
+    Method calls
+    ----------
+    :url - base URL
+    :id - primary key for the model
+    :obj - a single response item
+
+    GET     ":url"      -> [:obj, :obj, ... ]                   | status 200
+    GET     ":url/:id"  -> :obj                                 | status 200
+    POST    ":url"      -> :obj (the newly created object)      | status 201
+    PUT     ":url/:id"  -> :obj (the newly updated object)      | status 200
+    DELETE  ":url"      -> { "message": str }                   | status 204
+    DELETE  ":url/:id"  -> { "message": str }                   | status 204
     """
 
     queryset = Group.objects.all()
@@ -36,6 +97,34 @@ class GroupViewSet(viewsets.ModelViewSet):
 class RegisterAPI(generics.GenericAPIView):
     """
     API endpoint that allows a user to register.
+
+    Permissions
+    ----------
+    - Anyone can send POST requests
+
+    Response item
+    ----------
+    {
+        "id": int,
+        "last_login": str,
+        "is_superuser": bool,
+        "username": str,
+        "first_name": str,
+        "last_name": str,
+        "email": str,
+        "is_staff": bool,
+        "is_active": bool,
+        "date_joined": str,
+        "groups": [int, ..],
+        "user_permissions": [int, ..],
+    }
+
+    Method calls
+    ----------
+    :url - base URL
+    :obj - a single response item
+
+    POST    ":url"      -> {"user" :obj}      | status 201
     """
 
     permission_classes = (permissions.AllowAny,)
@@ -46,9 +135,6 @@ class RegisterAPI(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         user: User = serializer.save()
         return Response(
-            {
-                "user": UserSerializer(
-                    user, context=self.get_serializer_context()
-                ).data,
-            }
+            {"user": UserSerializer(user, context=self.get_serializer_context()).data,},
+            status=201,
         )
