2359efa940228b80cd66bd7d28eb53baf36074e2
Roberts Ivanovs <roberts.ivanovs1999@gmail.com>
2020-08-30 18:55:03 +0300

    Misc: Added docstrings to `education` app viewsets

--- backend/education/views.py
+++ backend/education/views.py
@@ -10,7 +10,47 @@ from education.permissions import (
 
 class ProgrammeViewset(viewsets.ModelViewSet):
     """
-    A viewset for viewing and editing Programme instances.
+    CRUD `education.models.Programme` items
+
+    Permissions
+    ----------
+    - Anyone can view the items, but people who have the set permissions to the
+        model can also operate with the items themselves
+    - Anyone can view the items, but only managers for the FK institituin can
+        edit it
+    ----------
+
+    Response item
+    ----------
+    {
+        "id": int,
+        "name": str,
+        "institution": int,
+        "degree": education.models.Programme.Degrees.choices,
+        "duration_years": int,
+        "faculty": str,
+        "learning_type": education.models.Programme.LearningTypes.choices,
+        "full_time": str,
+        "budget_places": int,
+        "total_places": int,
+        "study_costs": int,
+        "study_language": str,
+        "website": str,
+        "description": str,
+    }
+
+    Method calls
+    ----------
+    :url - base URL
+    :id - primary key for the model
+    :obj - a single response item
+
+    GET     ":url"      -> [:obj, :obj, ... ]                   | status 200
+    GET     ":url/:id"  -> :obj                                 | status 200
+    POST    ":url"      -> :obj (the newly created object)      | status 201
+    PUT     ":url/:id"  -> :obj (the newly updated object)      | status 200
+    DELETE  ":url"      -> { "message": str }                   | status 204
+    DELETE  ":url/:id"  -> { "message": str }                   | status 204
     """
 
     serializer_class = ProgrammeSerializer
@@ -23,7 +63,39 @@ class ProgrammeViewset(viewsets.ModelViewSet):
 
 class InstitutionViewset(viewsets.ModelViewSet):
     """
-    A viewset for viewing and editing Institution instances.
+    CRUD `education.models.Institution` items
+
+    Permissions
+    ----------
+    - Anyone can view the items, but people who have the set permissions to the
+        model can also operate with the items themselves
+    - Anyone can view the items, but only managers for the institituin can edit
+        it
+    ----------
+
+    Response item
+    ----------
+    {
+        "id": int,
+        "name": str,
+        "abbr": str,
+        "location": str,
+        "website": str,
+        "managers": [int, ..],
+    }
+
+    Method calls
+    ----------
+    :url - base URL
+    :id - primary key for the model
+    :obj - a single response item
+
+    GET     ":url"      -> [:obj, :obj, ... ]                   | status 200
+    GET     ":url/:id"  -> :obj                                 | status 200
+    POST    ":url"      -> :obj (the newly created object)      | status 201
+    PUT     ":url/:id"  -> :obj (the newly updated object)      | status 200
+    DELETE  ":url"      -> { "message": str }                   | status 204
+    DELETE  ":url/:id"  -> { "message": str }                   | status 204
     """
 
     serializer_class = InstitutionSerializer
