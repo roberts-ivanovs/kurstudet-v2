9e4a9278fd37524582a6dafbd6ec484854d1a838
Krystoart <krystoart@gmail.com>
2020-09-01 23:04:11 +0300

    Added function return types and changed out requester methods

--- frontend/src/global/UserContext.helper.ts
+++ frontend/src/global/UserContext.helper.ts
@@ -1,18 +1,24 @@
-import Requester from 'utils/Requester';
+import { Requester } from 'utils/Requester';
 import { UserTokens, User, AcessToken } from 'types';
+import { RegisterUserResponse } from 'requesterTypes';
 
 export const refreshTokenName = 'ks2refr';
 
-export function setRefreshToken(refreshToken: string) {
+export function setRefreshToken(refreshToken: string): void {
   // FIXME: Do not store refresh tokens in localstorage. use httpOnly cookies.
   // Currently the backend does not support that.
   window.localStorage.setItem(refreshTokenName, refreshToken);
 }
 
 export async function logIn(
-  username: string, password: string, cbAcessRaw: (arg0: string) => void,
-) {
-  const tokens: UserTokens = await Requester.post('/api/core/token/', { username, password });
+  username: string,
+  password: string,
+  cbAcessRaw: (arg0: string) => void,
+): Promise<void> {
+  const tokens: UserTokens = await Requester.getToken({
+    username,
+    password,
+  });
 
   Requester.setAuthHeader(tokens.access);
 
@@ -21,25 +27,35 @@ export async function logIn(
   window.localStorage.removeItem('logout');
 }
 
-export async function logOut(cb: (arg0: User | null) => void) {
+export async function logOut(cb: (arg0: User | null) => void): Promise<void> {
   Requester.setAuthHeader('');
   cb(null);
   window.localStorage.setItem('logout', Date.now().toString());
   window.localStorage.removeItem(refreshTokenName);
 }
 
-export async function getUser(userId: number, cb: (arg0: User) => void) {
-  const user: User = await Requester.get(`/api/core/user/${userId}/`);
+export async function getUser(
+  userId: number,
+  cb: (arg0: User) => void,
+): Promise<void> {
+  const user: User = await Requester.getUser({ userId });
   cb(user);
 }
 
 export async function refreshAcessToken(
-  refreshToken: string, setAcessToken: (arg0: string) => void,
-) {
-  const refreshed: AcessToken = await Requester.post('/api/core/token/refresh/', { refresh: refreshToken });
+  refreshToken: string,
+  setAcessToken: (arg0: string) => void,
+): Promise<void> {
+  const refreshed: AcessToken = await Requester.getTokenRefresh({
+    refresh: refreshToken,
+  });
   setAcessToken(refreshed.access);
 }
 
-export async function register(username: string, email: string, password: string): Promise<User> {
-  return Requester.post('/api/core/reigster/', { username, password, email });
+export async function register(
+  username: string,
+  email: string,
+  password: string,
+): Promise<RegisterUserResponse> {
+  return Requester.postRegisterUser({ username, password, email });
 }
