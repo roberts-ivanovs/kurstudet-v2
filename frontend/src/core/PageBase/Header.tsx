b179c1b222202e68b8fffd7d3b35f62d0b7eee7e
Krystoart <krystoart@gmail.com>
2020-09-04 00:35:58 +0300

    Created link to main page

--- frontend/src/core/PageBase/Header.tsx
+++ frontend/src/core/PageBase/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Header.scss';
+import { Link } from 'react-router-dom';
 
 export function Header(): React.ReactElement {
   return (
-    <div>
-      <nav>
-        smth
+    <div className="fixed-top navbar-box">
+      <nav className="center-vertical" role="navigation">
+        <Link
+          to={{ pathname: '/' }}
+        >
+          <li>
+            <div>
+              <span>Sākums</span>
+            </div>
+          </li>
+        </Link>
       </nav>
     </div>
   );
