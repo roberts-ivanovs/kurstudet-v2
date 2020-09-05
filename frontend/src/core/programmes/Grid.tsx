09581289b65e9fe57ae7c6bf0647d2862990b715
Roberts Ivanovs <roberts.ivanovs1999@gmail.com>
2020-09-05 18:15:13 +0300

    Frotnend: Removed `Props` from `Grid.tsx`

--- frontend/src/core/programmes/Grid.tsx
+++ frontend/src/core/programmes/Grid.tsx
@@ -1,10 +1,6 @@
 import React, { ReactElement } from 'react';
 
-interface Props {
-
-}
-
-export default function Grid({ }: Props): ReactElement {
+export function Grid(): ReactElement {
   return (
     <div />
   );
