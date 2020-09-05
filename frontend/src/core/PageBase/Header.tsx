722cd48bfad76952ed0687d5a58e8615bcb13cf7
Krystoart <krystoart@gmail.com>
2020-09-05 14:47:59 +0300

    Imported new StyleUtils.scss

--- frontend/src/core/PageBase/Header.tsx
+++ frontend/src/core/PageBase/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.scss';
+import '../../utils/StyleUtils.scss';
 import { Link } from 'react-router-dom';
 
 export function Header(): React.ReactElement {
