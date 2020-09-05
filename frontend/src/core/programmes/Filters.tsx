c6aeab87d757647b3989e87f84dfbe229e5c434f
Roberts Ivanovs <roberts.ivanovs1999@gmail.com>
2020-09-05 18:16:03 +0300

    Frontend: Created basic implementaiton of some filters

--- frontend/src/core/programmes/Filters.tsx
+++ frontend/src/core/programmes/Filters.tsx
@@ -1,11 +1,59 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+import { Institution, Programme } from 'types';
+import { SimpleFilter } from 'core/programmes/filter/SimpleFilter';
 
-interface Props {
+export function Filters(): ReactElement {
+  // Institutions
+  const [institutions, setInstitutions] = useState<Array<Institution>>([]);
+  const [institutionsSelected, setInstitutionsSelected] = useState<Array<Institution>>();
 
-}
+  // Programmes
+  const [programmes, setProgrammes] = useState<Array<Programme>>([]);
+  const [programmesSelected, setProgrammesSelected] = useState<Array<Programme>>();
 
-export default function Filters({}: Props): ReactElement {
+  // Full time or not
+  const fullTimeOptions = [
+    {
+      value: true,
+      label: 'Pilna laika',
+    },
+    {
+      value: false,
+      label: 'Nepilna laika',
+    },
+  ]
+  const [isFullTime, setisFullTime] = useState<Array<boolean>>([]);
+
+
   return (
-    <div />
+    <div>
+      <ul>
+        <li>
+          Institūcija
+          <SimpleFilter
+            options={institutions ? institutions?.map(
+              (el) => ({ value: el, label: el.name })
+            ) : []}
+            setActive={setInstitutionsSelected}
+          />
+        </li>
+        <li>
+          Programmas
+          <SimpleFilter
+            options={programmes ? programmes?.map(
+              (el) => ({ value: el, label: el.name })
+            ) : []}
+            setActive={setProgrammesSelected}
+          />
+        </li>
+        <li>
+          Pilna/nepilna laika
+          <SimpleFilter
+            options={fullTimeOptions}
+            setActive={setisFullTime}
+          />
+        </li>
+      </ul>
+    </div>
   );
 }
