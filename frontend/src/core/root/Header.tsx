f3630d515d43f027ca5e6f5c1232e287fa373509
krystoart <krystoart@gmail.com>
2020-09-07 09:49:30 +0300

    Added basic styling to the header component

--- frontend/src/core/root/Header.tsx
+++ frontend/src/core/root/Header.tsx
@@ -6,32 +6,52 @@ import style from './Header.module.scss';
 export function Header(): React.ReactElement {
   return (
     <div className={`${style['fixed-top']} ${style['navbar-box']}`}>
-      <nav className={styleUtil['center-vertical']} role="navigation">
-        <Link
-          to={{ pathname: '/' }}
-        >
-          Sākums
-        </Link>
-        <Link
-          to={{ pathname: 'programmes' }}
-        >
-          Programmas
-        </Link>
-        <Link
-          to={{ pathname: 'forum' }}
-        >
-          Forums
-        </Link>
-        <Link
-          to={{ pathname: 'about' }}
-        >
-          Par
-        </Link>
-        <Link
-          to={{ pathname: 'connect' }}
-        >
-          Pieslēgties
-        </Link>
+      <nav className={`${styleUtil['center-vertical']} ${style['header-wrapper']}`} role="navigation">
+        <div className={`${style['navbar-link']} ${style['navbar-first-item']}`}>
+          <Link
+            to={{ pathname: '/' }}
+          >
+            <p className={style['navbar-text']}>
+              Sākums
+            </p>
+          </Link>
+        </div>
+        <div className={style['navbar-link']}>
+          <Link
+            to={{ pathname: 'programmes' }}
+          >
+            <p className={style['navbar-text']}>
+              Programmas
+            </p>
+          </Link>
+        </div>
+        <div className={style['navbar-link']}>
+          <Link
+            to={{ pathname: 'forum' }}
+          >
+            <p className={style['navbar-text']}>
+              Forums
+            </p>
+          </Link>
+        </div>
+        <div className={style['navbar-link']}>
+          <Link
+            to={{ pathname: 'about' }}
+          >
+            <p className={style['navbar-text']}>
+              Par
+            </p>
+          </Link>
+        </div>
+        <div className={style['navbar-button']}>
+          <Link
+            to={{ pathname: 'connect' }}
+          >
+            <p className={style['navbar-button-text']}>
+              Pieslēgties
+            </p>
+          </Link>
+        </div>
       </nav>
     </div>
   );
