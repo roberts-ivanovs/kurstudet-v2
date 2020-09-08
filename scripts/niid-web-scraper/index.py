90d8a627155e84ba64e7ded8ed73e0d247f706f8
Arina Solovjova <arinacka2@gmail.com>
2020-09-08 21:02:47 +0300

    Scripts: Webscraper: Base for csv file

--- scripts/niid-web-scraper/index.py
+++ scripts/niid-web-scraper/index.py
@@ -1,1 +1,6 @@
-# Write your code here
+from csv import writer
+
+with open('Institutions.csv', 'w') as csv_file:
+    csv_writer = writer(csv_file)
+    headers = ['name', 'abbr', 'location', 'website']
+    csv_writer.writerow(headers)
