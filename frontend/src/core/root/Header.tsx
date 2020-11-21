bd5289b5b18fa6f77fd9ff7bfee7633bdcccad57
krystoart <krystoart@gmail.com>
2020-11-21 21:03:28 +0200

    Frontend: some wip stuff

--- frontend/src/core/root/Header.tsx
+++ frontend/src/core/root/Header.tsx
@@ -5,54 +5,56 @@ import style from './Header.module.scss';
 
 export function Header(): React.ReactElement {
   return (
-    <div className={`${style['fixed-top']} ${style['navbar-box']}`}>
-      <nav className={`${styleUtil['center-vertical']} ${style['header-wrapper']}`} role="navigation">
-        <div className={`${style['navbar-link']} ${style['navbar-first-item']}`}>
-          <Link
-            to={{ pathname: '/' }}
-          >
-            <p className={style['navbar-text']}>
-              Sākums
-            </p>
-          </Link>
-        </div>
-        <div className={style['navbar-link']}>
-          <Link
-            to={{ pathname: 'programmes' }}
-          >
-            <p className={style['navbar-text']}>
-              Programmas
-            </p>
-          </Link>
-        </div>
-        <div className={style['navbar-link']}>
-          <Link
-            to={{ pathname: 'forum' }}
-          >
-            <p className={style['navbar-text']}>
-              Forums
-            </p>
-          </Link>
-        </div>
-        <div className={style['navbar-link']}>
-          <Link
-            to={{ pathname: 'about' }}
-          >
-            <p className={style['navbar-text']}>
-              Par
-            </p>
-          </Link>
-        </div>
-        <div className={style['navbar-button']}>
-          <Link
-            to={{ pathname: 'connect' }}
-          >
-            <p className={style['navbar-button-text']}>
-              Pieslēgties
-            </p>
-          </Link>
-        </div>
+    <div className={`${style['fixed-top']} ${style['navbar-box']} ${styleUtil['center-vertical']}`}>
+      <nav className={`${style['header-wrapper']} ${styleUtil['center-vertical']} `} role="navigation">
+        <ul className={style['list-box']}>
+          <li className={`${style['navbar-link']} ${style['navbar-first-item']}`}>
+            <Link
+              to={{ pathname: '/' }}
+            >
+              <p className={`${style['navbar-text']} ${style['navbar-underline']}`}>
+                Sākums
+              </p>
+            </Link>
+          </li>
+          <li className={style['navbar-link']}>
+            <Link
+              to={{ pathname: 'programmes' }}
+            >
+              <p className={style['navbar-text']}>
+                Programmas
+              </p>
+            </Link>
+          </li>
+          <li className={style['navbar-link']}>
+            <Link
+              to={{ pathname: 'forum' }}
+            >
+              <p className={style['navbar-text']}>
+                Forums
+              </p>
+            </Link>
+          </li>
+          <li className={style['navbar-link']}>
+            <Link
+              to={{ pathname: 'about' }}
+            >
+              <p className={style['navbar-text']}>
+                Par
+              </p>
+            </Link>
+          </li>
+        </ul>
       </nav>
+      <div className={style['navbar-button']}>
+        <Link
+          to={{ pathname: 'connect' }}
+        >
+          <button type="button" className={style['navbar-button-text']}>
+            Pieslēgties
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
